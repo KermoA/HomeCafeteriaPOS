74ed65eefdb62615440326b03a526f3578050a39
DESKTOP-FSFG20J\Kermo <kermoanijarv@gmail.com>
2025-03-07 19:35:00 +0200

    Old Frontend project removed

--- HomeCafeteriaPOS/HomeCafeteriaPOS.Frontend/Models/Product.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace HomeCafeteriaPOS.Frontend.Models
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public decimal Price { get; set; }
-    public int Quantity { get; set; }
-}
